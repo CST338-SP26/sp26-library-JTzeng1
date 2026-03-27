0f77896f12b1d85f95730abbeb44db83a6d5cb70
JTzeng1 <jtzeng@csumb.edu>
2026-03-26 21:03:42 -0700

    added all fields

---
+++ src/main/java/Reader.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Reader {
+    public static final int cardNumber_ = 0;
+    public static final int NAME_ = 1;
+    public static final int PHONE_ = 2;
+    public static final int BOOK_COUNT_ =3;
+    public static final int BOOK_START = 4;
+
+    private String cardNumber;
+    private String name;
+    private String phone;
+    private List<Book> books;
+
+
+
+
+
+}
