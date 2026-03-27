dfd14ad6ed85d277f9baa9a28abb1dcb0100aa4b
JTzeng1 <jtzeng@csumb.edu>
2026-03-26 22:27:50 -0700

    fixed typos added toString added addBook added removeBook
    fixed equals
    added hashCode

--- src/test/java/ReaderTest.java
+++ src/test/java/ReaderTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
